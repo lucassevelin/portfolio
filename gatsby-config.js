a3d2ed4e3c57d2685420973e9df574dd5c58bc6d
Lucas Sevelin <lucas.sevelin@gmail.com>
2022-04-15 11:02:02 +0200

    Rebranding
    Changed the template branding to my own

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Lucas Sevein`,
+    siteTitleAlt: `Lucas Sevelin - Portfolio`,
+    siteHeadline: `Lucas Sevelin - Portfolio`,
+    siteUrl: `https://lucassevelin.com`,
+    siteDescription: `Portfolio for Lucas Sevelin - a hobby developer`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `Lucas Sevelin`,
   },
   plugins: [
     {
